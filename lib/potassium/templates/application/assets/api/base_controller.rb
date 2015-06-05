def9199037eea1bef0e3ed1f0ad9113ec18b43a3
Emilio Blanco <emilioeduardob@gmail.com>
2015-06-05 11:38:44 -0400

    feat(): Add responders gem to api recipe

--- lib/potassium/templates/application/assets/api/base_controller.rb
+++ lib/potassium/templates/application/assets/api/base_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::BaseController < ApplicationController
+  self.responder = ApiResponder
+
+  respond_to :json
 end
--- lib/potassium/templates/application/recipes/api.rb
+++ lib/potassium/templates/application/recipes/api.rb
@@ -1,16 +1,17 @@
 if get(:api_support)
   gather_gem 'versionist'
+  gather_gem 'responders'
 
   after(:gem_install) do
-    line = "Rails.application.routes.draw do"
-    routes = "config/routes.rb"
-    gsub_file routes, /(#{Regexp.escape(line)})/mi do |match|
-      <<-HERE.gsub(/^ {9}/, '')
+    line = "Rails.application.routes.draw do\n"
+    insert_into_file "config/routes.rb", after: line do
+      <<-HERE.gsub(/^ {7}/, '')
          api_version(:module => "Api::V1", :path => {:value => "v1"}) do
          end
-         HERE
+      HERE
     end
 
-    copy_file 'assets/api/base_controller.rb', 'app/controllers/api/v1/base_controller'
+    copy_file 'assets/api/base_controller.rb', 'app/controllers/api/v1/base_controller.rb'
+    copy_file 'assets/api/responder.rb', 'app/responders/api_responder.rb'
   end
 end
