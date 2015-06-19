931ef06b445cf200815458715f857bf180563695
Emilio Blanco <emilioeduardob@gmail.com>
2015-06-19 15:25:43 -0400

    fix(): Process all callbacks

--- lib/potassium/templates/application/recipe_template.rb
+++ lib/potassium/templates/application/recipe_template.rb
@@ -1,14 +1,23 @@
 recipe = ARGV.first
 
-def selected?(key, val = nil)
-  true
-end
+# Consider all marked as true
+def selected?(key, val = nil); true; end
 
 run_action(:recipe_loading) do
   eval_file "recipes/#{recipe}.rb"
 end
 
+
 run_action(:gem_install) do
   build_gemfile
   run "bundle install"
+end
+
+get(:callbacks).each do |name, cbs|
+
+  puts "Processing #{name} callbacks"
+  cbs.each do |event, cb|
+    cb.each {|callback| instance_exec(&callback) }
+  end
+
 end
