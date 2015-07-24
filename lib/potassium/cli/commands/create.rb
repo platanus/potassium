0a8d874c0ef4b8f47ab2ddffeea9f79621009ca1
juliogarciag <julioggonz@gmail.com>
2015-07-24 13:58:27 -0500

    feat(version_check): Checking version of potassium previous to installation

--- lib/potassium/cli/commands/create.rb
+++ lib/potassium/cli/commands/create.rb
@@ -4,13 +4,18 @@ module Potassium::CLI
   command :create do |c|
     c.default_desc "Create a new project."
     c.action do |global_options, options, args|
-      require "potassium/templates/application/generator"
-      require "potassium/template_finder"
+      require "potassium/newest_version_ensurer"
 
-      template_finder = Potassium::TemplateFinder.new
-      template = template_finder.default_template
-      template.source_paths << Rails::Generators::AppGenerator.source_root
-      template.start
+      ensurer = Potassium::NewestVersionEnsurer.new
+      ensurer.ensure do
+        require "potassium/templates/application/generator"
+        require "potassium/template_finder"
+
+        template_finder = Potassium::TemplateFinder.new
+        template = template_finder.default_template
+        template.source_paths << Rails::Generators::AppGenerator.source_root
+        template.start
+      end
     end
   end
 end
