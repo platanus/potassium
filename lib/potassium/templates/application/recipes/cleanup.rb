f484fcba5b10d681645b154c9510295c572e4824
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-02-04 13:53:16 -0500

    feat(): cleanup config files
    Removes comments from application and environment configuration files

---
+++ lib/potassium/templates/application/recipes/cleanup.rb
@@ -0,0 +1,5 @@
+erase_comments "config/application.rb"
+erase_comments "config/environments/production.rb"
+erase_comments "config/environments/staging.rb"
+erase_comments "config/environments/test.rb"
+erase_comments "config/environments/development.rb"
--- lib/potassium/templates/application/template.rb
+++ lib/potassium/templates/application/template.rb
@@ -42,6 +42,7 @@ run_action(:recipe_loading) do
   eval_file "recipes/api.rb"
   eval_file "recipes/rack-cors.rb"
   eval_file "recipes/paperclip.rb"
+  eval_file "recipes/cleanup.rb"
 end
 
 say "Gathered enough information. Applying the template. Wait a minute.", :green
