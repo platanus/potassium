146474cb6e18a5b1668b97e160d7daf3f09380a5
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-02-04 22:03:37 -0500

    feat(heroku): Adds the bower buildpack
    This buildpack ensure bower is installed and will run bower install command to fetch all the app dependencies.

--- lib/potassium/templates/application/recipes/bower.rb
+++ lib/potassium/templates/application/recipes/bower.rb
@@ -1,3 +1,9 @@
 copy_file 'assets/.bowerrc', '.bowerrc'
 template 'assets/bower.json', 'bower.json'
 application "config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')"
+
+if get(:heroku)
+  bower_buildpack_url = 'https://github.com/platanus/heroku-buildpack-bower.git'
+  insert_point = 'https://github.com/platanus/heroku-buildpack-ruby-version.git'
+  inject_into_file '.buildpacks', "#{bower_buildpack_url}\n", before: insert_point
+end
