e2142099480372d0783605d42f03a78f4700b822
Leandro Segovia <ldlsegovia@gmail.com>
2016-04-28 14:51:26 -0300

    feat(): add /public/system dir to gitignore

--- lib/potassium/recipes/paperclip.rb
+++ lib/potassium/recipes/paperclip.rb
@@ -32,6 +32,7 @@ class Recipes::Paperclip < Rails::AppBuilder
     application paperclip_config.strip, env: 'production'
     append_to_file '.env.example', 'AWS_BUCKET='
     append_to_file '.env', 'AWS_BUCKET='
+    append_to_file '.gitignore', "/public/system/*\n"
     add_readme_section :internal_dependencies, :paperclip
   end
 end
