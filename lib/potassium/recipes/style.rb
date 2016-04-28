644f25a5114076667f6f0a3e7e65c20f307d7a66
Leandro Segovia <ldlsegovia@gmail.com>
2016-04-28 14:50:48 -0300

    fix(): \n does not work with single quotes

--- lib/potassium/recipes/style.rb
+++ lib/potassium/recipes/style.rb
@@ -3,7 +3,7 @@ class Recipes::Style < Rails::AppBuilder
     copy_file '../assets/.rubocop.yml', '.rubocop.yml'
     copy_file '../assets/.ruby_style.yml', '.ruby_style.yml'
     copy_file '../assets/.hound.yml', '.hound.yml'
-    append_to_file '.gitignore', '.rubocop-http*\n'
+    append_to_file '.gitignore', ".rubocop-http*\n"
     add_readme_header :style_guide
   end
 
