e32e65b499b92baff4d0080291424544c4943d4f
Arturo Puente <arturopuentevc@gmail.com>
2016-06-24 12:15:10 -0500

    Remove old hound config files

--- lib/potassium/recipes/style.rb
+++ lib/potassium/recipes/style.rb
@@ -1,8 +1,8 @@
 class Recipes::Style < Rails::AppBuilder
   def create
-    append_to_file '.gitignore', ".rubocop.yml\n"
-    append_to_file '.gitignore', ".eslintrc.json\n"
-    append_to_file '.gitignore', ".sscs-lint.yml\n"
+    append_to_file ".gitignore", ".rubocop.yml\n"
+    append_to_file ".gitignore", ".eslintrc.json\n"
+    append_to_file ".gitignore", ".sscs-lint.yml\n"
     add_readme_header :style_guide
   end
 
