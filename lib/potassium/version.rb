9ec2fc0fcc13a118dc0794fa1d73790270c8ec67
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-09-26 15:45:05 -0400

    chore(): update some version

--- lib/potassium/version.rb
+++ lib/potassium/version.rb
@@ -1,6 +1,6 @@
 module Potassium
-  VERSION = "2.1.0"
+  VERSION = "2.2.0"
   RUBY_VERSION = "2.3.1"
   RAILS_VERSION = "~> 4.2.0"
-  RUBOCOP_VERSION = "~> 0.38.0"
+  RUBOCOP_VERSION = "~> 0.42.0"
 end
