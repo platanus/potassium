379403ff093474bd2b40d49dbd925e121882fd85
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-12-15 18:24:34 -0500

    feat(mails): adds mailer recipe
    - AWS SES support
    - Sendgrid Beta support
    - Mail interceptor
    - Use backaground jobs

--- lib/potassium/recipes/production.rb
+++
@@ -1,7 +0,0 @@
-class Recipes::Production < Rails::AppBuilder
-  def create
-    gsub_file "config/environments/production.rb", /(\# config.action_mailer.+)/i do |match|
-      "#{match}\n  config.action_mailer.default_options = { from: ENV['DEFAULT_EMAIL_ADDRESS'] }\n"
-    end
-  end
-end
