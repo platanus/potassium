ae0f313d24867f06fcdcbf41f4dde91203f5f31c
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-12-15 18:21:31 -0500

    feat(aws): add update aws-sdk and paperclip
    - Adds region as a default environmental variable
    - Change bucket environmental variable to `S3_BUCKET`

--- lib/potassium/recipes/paperclip.rb
+++ lib/potassium/recipes/paperclip.rb
@@ -25,12 +25,12 @@ class Recipes::Paperclip < Rails::AppBuilder
          config.paperclip_defaults = {
            storage: :s3,
            s3_credentials: {
-             bucket: ENV['AWS_BUCKET']
+             bucket: ENV['S3_BUCKET']
            }
          }
          RUBY
     application paperclip_config.strip, env: 'production'
-    append_to_file '.env.development', 'AWS_BUCKET='
+    append_to_file '.env.development', "S3_BUCKET=\n"
     append_to_file '.gitignore', "/public/system/*\n"
     add_readme_section :internal_dependencies, :paperclip
   end
