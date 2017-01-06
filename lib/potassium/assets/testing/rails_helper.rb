81dcd422068e4d70c8f448a0241d841b95179579
Leandro Segovia <ldlsegovia@gmail.com>
2017-01-06 17:13:20 -0300

    feat(testing): add faker gem

--- lib/potassium/assets/testing/rails_helper.rb
+++ lib/potassium/assets/testing/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'spec_helper'
 require 'shoulda/matchers'
+require 'faker'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
