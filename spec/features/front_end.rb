62aa8e8f93f9b117769c91081374a6d128733511
Camilo Flores <camilo.ignacio@gmail.com>
2017-04-07 16:16:03 -0300

    task(front_end): fix typo

--- spec/features/front_end.rb
+++ spec/features/front_end.rb
@@ -4,13 +4,13 @@ RSpec.describe "Front end" do
   before(:all) { drop_dummy_database }
   before(:each) { remove_project_directory }
 
-  it "creates a project wihtout a front end framework" do
+  it "creates a project without a front end framework" do
     create_dummy_project("front_end" => "None")
     gemfile = IO.read("#{project_path}/Gemfile")
     expect(gemfile).not_to include('webpacker')
   end
 
-  it "creates a project wihtout vue as front end framework" do
+  it "creates a project without vue as front end framework" do
     create_dummy_project("front_end" => "angular")
     gemfile = IO.read("#{project_path}/Gemfile")
     node_modules_file = IO.read("#{project_path}/package.json")
@@ -19,7 +19,7 @@ RSpec.describe "Front end" do
     expect(node_modules_file).to include("\"@angular/core\"")
   end
 
-  it "creates a project wihtout vue as front end framework" do
+  it "creates a project without vue as front end framework" do
     create_dummy_project("front_end" => "vue")
     gemfile = IO.read("#{project_path}/Gemfile")
     node_modules_file = IO.read("#{project_path}/package.json")
