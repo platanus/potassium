b93231368879004cdf9eafe746cc8dac98c428a2
Camilo Flores <camilo.ignacio@gmail.com>
2017-05-19 17:37:27 -0400

    task(rails): update rails to 5.1.1

--- lib/potassium/version.rb
+++ lib/potassium/version.rb
@@ -1,6 +1,6 @@
 module Potassium
-  VERSION = "5.1.0"
+  VERSION = "5.1.1"
   RUBY_VERSION = "2.3.1"
-  RAILS_VERSION = "~> 5.1.0.rc1"
+  RAILS_VERSION = "~> 5.1.1"
   RUBOCOP_VERSION = "~> 0.46.0"
 end
