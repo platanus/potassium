021d4f9d76179c2e36c71058fcab1e0c155e7761
Juan Camilo Hernández <camilo9020@gmail.com>
2017-05-19 16:49:22 -0700

    test(schedule): Add tests to schedule recipe

--- spec/features/background_processor_spec.rb
+++ spec/features/background_processor_spec.rb
@@ -1,50 +1,6 @@
 require "spec_helper"
 
 RSpec.describe "BackgroundProcessor" do
-  context "working with delayed job" do
-    before :all do
-      drop_dummy_database
-      remove_project_directory
-      create_dummy_project("background_processor" => "delayed_job", "heroku" => true)
-    end
-
-    it "adds delayed_job gem to Gemfile" do
-      gemfile_content = IO.read("#{project_path}/Gemfile")
-      expect(gemfile_content).to include("gem 'delayed_job_active_record'")
-    end
-
-    it "adds queue_adapter to application.rb" do
-      content = IO.read("#{project_path}/config/application.rb")
-      expect(content).to include("config.active_job.queue_adapter = :delayed_job")
-    end
-
-    it "adds inline queue_adapter to development.rb" do
-      content = IO.read("#{project_path}/config/environments/development.rb")
-      expect(content).to include("config.active_job.queue_adapter = :inline")
-    end
-
-    it "adds test queue_adapter to test.rb" do
-      content = IO.read("#{project_path}/config/environments/test.rb")
-      expect(content).to include("config.active_job.queue_adapter = :test")
-    end
-
-    it "modifies Procfile" do
-      content = IO.read("#{project_path}/Procfile")
-      expect(content).to include("worker: bundle exec rails jobs:work")
-    end
-
-    it "modifies README" do
-      content = IO.read("#{project_path}/README.md")
-      expect(content).to include("this project uses [DelayedJob]")
-    end
-
-    it "overrides migration" do
-      migration_name = Dir.entries("#{project_path}/db/migrate").last
-      content = IO.read("#{project_path}/db/migrate/#{migration_name}")
-      expect(content).to include("CreateDelayedJobs < ActiveRecord::Migration[4.2]")
-    end
-  end
-
   context "working with sidekiq" do
     before :all do
       drop_dummy_database
