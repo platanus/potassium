6509a295355ca87d003df352a7c48b68fd3f12cc
Guillermo Moreno <gmq@guillermomoreno.cl>
2017-06-23 16:37:13 -0400

    fix(app): execute git after bundle install and binstubs generation

--- lib/potassium/generators/application.rb
+++ lib/potassium/generators/application.rb
@@ -15,6 +15,12 @@ module Potassium
       template_location = File.expand_path('../templates/application.rb', File.dirname(__FILE__))
       instance_eval File.read(template_location), template_location
       super
+
+      after_bundle do
+        git :init
+        git add: "."
+        git commit: %{ -m 'Initial commit' -q }
+      end
     end
   end
 end
--- lib/potassium/recipes/git.rb
+++
@@ -1,9 +0,0 @@
-class Recipes::Git < Rails::AppBuilder
-  def create
-    git :init
-    after(:database_creation) do
-      git add: "."
-      git commit: %{ -m 'Initial commit' }
-    end
-  end
-end
