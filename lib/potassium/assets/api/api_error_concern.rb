272349818b65783f95c7ebdcdf632d700b382983
Juan Camilo Hernández <camilo9020@gmail.com>
2018-02-16 11:57:49 -0500

    feat(assets/api): Improve ApiErrorConcern

--- lib/potassium/assets/api/api_error_concern.rb
+++ lib/potassium/assets/api/api_error_concern.rb
@@ -5,28 +5,39 @@ module ApiErrorConcern
     rescue_from "Exception" do |exception|
       logger.error exception.message
       logger.error exception.backtrace.join("\n")
-      respond_api_error(:internal_server_error, message: "server_error",
-                                                type: exception.class.to_s,
-                                                detail: exception.message)
+      respond_api_error(500, "server_error", exception.class.to_s)
+    end
+
+    rescue_from "ActionController::UnknownFormat" do |exception|
+      respond_api_error(404, "unknown_format", exception.class.to_s)
     end
 
     rescue_from "ActiveRecord::RecordNotFound" do |exception|
-      respond_api_error(:not_found, message: "record_not_found",
-                                    detail: exception.message)
+      respond_api_error(404, "record_not_found", exception.class.to_s)
     end
 
     rescue_from "ActiveModel::ForbiddenAttributesError" do |exception|
-      respond_api_error(:bad_request, message: "protected_attributes",
-                                      detail: exception.message)
+      respond_api_error(400, "protected_attributes", exception.class.to_s)
     end
 
     rescue_from "ActiveRecord::RecordInvalid" do |exception|
-      respond_api_error(:bad_request, message: "invalid_attributes",
-                                      errors: exception.record.errors)
+      respond_api_error(400, "invalid_attributes", exception.record.errors)
+    end
+
+    rescue_from "ApiVersionError" do
+      data = ["application/json; version=1", "application/json; version=2"]
+      respond_api_error(406, "invalid_api_version", data)
     end
   end
 
-  def respond_api_error(status, error = {})
-    render json: error, status: status
+  def respond_api_error(code, message, data)
+    error = {
+      code: code,
+      status: code >= 500 ? "fail" : "error",
+      message: message,
+      data: data
+    }
+
+    render json: error, status: code
   end
 end
