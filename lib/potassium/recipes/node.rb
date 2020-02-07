5e27893bd02209127190f165ef22bbe9d53c6c3b
LuisFros <lmfros@uc.cl>
2020-02-07 18:57:36 -0300

    chore(): add node-version to potasssium

--- lib/potassium/recipes/node.rb
+++ lib/potassium/recipes/node.rb
@@ -7,10 +7,9 @@ class Recipes::Node < Rails::AppBuilder
   def create
     info "Using node version #{version_alias}"
     create_file '.node-version', version_alias, force: true
-    byebug
     json_file = File.read(Pathname.new("package.json"))
     js_package = JSON.parse(json_file)
-    js_package["engines"] = {"node"=> "#{version}"}
+    js_package["engines"] = { "node" => version.to_s }
     json_string = JSON.pretty_generate(js_package)
     create_file 'package.json', json_string, force: true
   end
