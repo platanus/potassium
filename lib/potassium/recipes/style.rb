ad8b7282dfd2385cf21614d650fa5be1ee2ad196
Raimundo Herrera <rjherrera@uc.cl>
2020-04-17 19:39:50 -0400

    feat(style): add per-project linting configs and dependencies

--- lib/potassium/recipes/style.rb
+++ lib/potassium/recipes/style.rb
@@ -1,12 +1,30 @@
 class Recipes::Style < Rails::AppBuilder
   def create
-    append_to_file ".gitignore", ".rubocop.yml\n"
-    append_to_file ".gitignore", ".eslintrc.json\n"
-    append_to_file ".gitignore", ".sscs-lint.yml\n"
+    add_linters
+    add_config_files
     add_readme_header :style_guide
   end
 
   def install
     create
+  end
+
+  private
+
+  def add_linters
+    gather_gems(:development, :test) do
+      gather_gem 'rubocop', '~> 0.82.0'
+      gather_gem 'rubocop-performance'
+      gather_gem 'rubocop-rails'
+      gather_gem 'rubocop-rspec'
+    end
+    run 'bin/yarn add --dev stylelint eslint eslint-plugin-import'
+    run 'bin/yarn add --dev eslint-plugin-vue' if selected?(:front_end, :vue)
+  end
+
+  def add_config_files
+    copy_file '../assets/.rubocop.yml', '.rubocop.yml'
+    copy_file '../assets/.eslintrc.json', '.eslintrc.json'
+    copy_file '../assets/.stylelintrc.json', '.stylelintrc.json'
   end
 end
