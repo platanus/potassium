02bd9c2bd653b8d6cf5d4f6769655dd17f7ec38b
Diego Fernández <difernandez@uc.cl>
2020-05-08 18:12:51 -0400

    fix(base-uploader): access allowed_types from uploader, not attacher

--- lib/potassium/assets/app/uploaders/base_uploader.rb
+++ lib/potassium/assets/app/uploaders/base_uploader.rb
@@ -2,10 +2,8 @@ class BaseUploader < Shrine
   plugin :validation_helpers
 
   Attacher.validate do
-    validate_mime_type allowed_types
+    validate_mime_type store.allowed_types
   end
-
-  private
 
   def allowed_types
     raise NotImplementedError
