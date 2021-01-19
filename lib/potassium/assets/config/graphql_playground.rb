31775e032db9d16dfb2ccee3186019b42f2972cd
ironcadiz <afcadiz@uc.cl>
2021-01-19 14:41:14 -0300

    fix(gql): Fix production server not running due to config of dev gem

--- lib/potassium/assets/config/graphql_playground.rb
+++ lib/potassium/assets/config/graphql_playground.rb
@@ -1,18 +1,20 @@
 # config/initializers/graphql_playground.rb
 # All config options have a default that should work out of the box
-GraphqlPlayground::Rails.configure do |config|
-  # config.headers = {
-  #   'X-Auth-Header' => ->(view_context) { "123" }
-  # }
-  # config.title = "Playground"
-  # config.csrf = true
-  # config.playground_version = "latest"
-  # # Ideally the assets would be added to your projects `vendor/assets` directories
-  # config.favicon = "/assets/playground.ico"
-  # config.playground_js_url = "/assets/playground.js"
-  # config.playground_css_url = "/assets/playground.css"
-  # # see: https://github.com/prisma-labs/graphql-playground#settings
-  config.settings = {
-    "schema.polling.enable": false
-  }
+if Rails.env.development?
+  GraphqlPlayground::Rails.configure do |config|
+    # config.headers = {
+    #   'X-Auth-Header' => ->(view_context) { "123" }
+    # }
+    # config.title = "Playground"
+    # config.csrf = true
+    # config.playground_version = "latest"
+    # # Ideally the assets would be added to your projects `vendor/assets` directories
+    # config.favicon = "/assets/playground.ico"
+    # config.playground_js_url = "/assets/playground.js"
+    # config.playground_css_url = "/assets/playground.css"
+    # # see: https://github.com/prisma-labs/graphql-playground#settings
+    config.settings = {
+      "schema.polling.enable": false
+    }
+  end
 end
