fd748d71b1de90138462f6630c251da594666088
Diego Fernández <difernandez@uc.cl>
2021-01-27 11:28:31 -0300

    chore(): use rubocop version 0.82, add local config

--- lib/potassium/version.rb
+++ lib/potassium/version.rb
@@ -2,7 +2,7 @@ module Potassium
   VERSION = "6.1.0"
   RUBY_VERSION = "2.7.0"
   RAILS_VERSION = "~> 6.0.2"
-  RUBOCOP_VERSION = "~> 0.65.0"
+  RUBOCOP_VERSION = "~> 0.82.0"
   POSTGRES_VERSION = "11.3"
   MYSQL_VERSION = "5.7"
   NODE_VERSION = "12"
