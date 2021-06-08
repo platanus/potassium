f89cea7da5cdaba32879ae3be86ffe613c42d070
Raimundo Herrera <rjherrera@uc.cl>
2021-06-08 16:34:20 -0400

    test(ci): update spec to reflect new ci flow with cache

--- spec/features/ci_spec.rb
+++ spec/features/ci_spec.rb
@@ -1,14 +1,17 @@
 require "spec_helper"
 require "rubocop"
 
-RSpec.describe "A new project" do
+RSpec.describe 'CI' do
   before(:all) do
     drop_dummy_database
     remove_project_directory
-    create_dummy_project("heroku" => true)
+    create_dummy_project
   end
 
-  it "correctly runs continous integration" do
-    expect { on_project { `bin/cibuild` } }.to_not output.to_stderr
+  it "correctly bundles the config file" do
+    yml_path = "#{project_path}/.circleci/config.yml"
+    content = IO.read(yml_path)
+    expect(File.exist?(yml_path)).to be true
+    expect(content).to include('circleci/ruby', 'cache', 'rspec', 'reviewdog')
   end
 end
