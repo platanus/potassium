fc9776b4bb603114fc8e79a84b61bd3e53d7d674
Leandro Segovia <ldlsegovia@gmail.com>
2021-10-04 11:36:39 -0300

    feat(recipes/testing): add integration test helpers to devise config

--- lib/potassium/assets/testing/devise_config.rb
+++ lib/potassium/assets/testing/devise_config.rb
@@ -2,4 +2,5 @@ require 'devise'
 
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
