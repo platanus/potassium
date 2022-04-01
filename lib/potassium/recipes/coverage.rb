01c339b7a5839578ebb5a4a2d61b4040cd6e3869
FloValdes <ftvaldes1@uc.cl>
2022-04-01 18:26:13 -0300

    feat(coverage-recipe): add front end coverage for jest

--- lib/potassium/recipes/coverage.rb
+++ lib/potassium/recipes/coverage.rb
@@ -3,6 +3,10 @@ class Recipes::Coverage < Rails::AppBuilder
     load_gems
     configure_rails_helper
     append_to_file('.gitignore', "/coverage/*\n")
+    recipe = self
+    after(:setup_jest) do
+      recipe.configure_jest_coverage
+    end
   end
 
   def installed?
@@ -11,6 +15,14 @@ class Recipes::Coverage < Rails::AppBuilder
 
   def install
     create
+  end
+
+  def configure_jest_coverage
+    json_file = File.read(Pathname.new("package.json"))
+    js_package = JSON.parse(json_file)
+    js_package = add_coverage_config(js_package)
+    json_string = JSON.pretty_generate(js_package)
+    create_file 'package.json', json_string, force: true
   end
 
   private
@@ -31,5 +43,19 @@ class Recipes::Coverage < Rails::AppBuilder
         "#{match}\nrequire 'simplecov_config'"
       end
     end
+  end
+
+  def add_coverage_config(js_package)
+    js_package['scripts']['test:changes'] = 'jest --changedSince=master'
+    js_package['jest'] = js_package['jest'].merge(coverage_defaults)
+    js_package
+  end
+
+  def coverage_defaults
+    {
+      collectCoverage: true,
+      collectCoverageFrom: ['**/*.{js,ts,vue}', '!**/node_modules/**'],
+      coverageReporters: ['text']
+    }
   end
 end
