491712317d3002972b45a941ee61754ddc7825aa
Leandro Segovia <ldlsegovia@gmail.com>
2023-02-21 13:58:24 -0300

    feat(recipes/ci): install repo analyzer

--- spec/features/ci_spec.rb
+++ spec/features/ci_spec.rb
@@ -2,6 +2,11 @@ require "spec_helper"
 require "rubocop"
 
 RSpec.describe 'CI' do
+  let(:ci_config) do
+    yml_path = "#{project_path}/.circleci/config.yml"
+    IO.read(yml_path)
+  end
+
   before(:all) do
     drop_dummy_database
     remove_project_directory
@@ -9,9 +14,10 @@ RSpec.describe 'CI' do
   end
 
   it "correctly bundles the config file" do
-    yml_path = "#{project_path}/.circleci/config.yml"
-    content = IO.read(yml_path)
-    expect(File.exist?(yml_path)).to be true
-    expect(content).to include('cimg/ruby', 'cache', 'rspec', 'reviewdog')
+    expect(ci_config).to include('cimg/ruby', 'cache', 'rspec', 'reviewdog')
+  end
+
+  it "adds repo analyzer config" do
+    expect(ci_config).to include('bin/rake "repo_analyzer:analyze[platanus/dummy_app]"')
   end
 end
