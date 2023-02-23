a351f18d4e0c970622993f1e488f5da8570ba9c0
Guillermo Moreno <gmq@guillermomoreno.cl>
2023-02-23 15:51:08 -0300

    feat(vite): add missing vite dev content security

--- lib/potassium/recipes/front_end_vite.rb
+++ lib/potassium/recipes/front_end_vite.rb
@@ -17,7 +17,25 @@ class Recipes::FrontEndVite < Rails::AppBuilder
     after(:gem_install, wrap_in_action: :vite_install) do
       run "yarn install"
       run "bundle exec vite install"
+      recipe.add_vite_dev_ws_content_security_policy
       recipe.copy_dotenv_monkeypatch
+    end
+  end
+
+  def add_vite_dev_ws_content_security_policy
+    initializer = "config/initializers/content_security_policy.rb"
+    line = "#    policy.style_src *policy.style_src, :unsafe_inline if Rails.env.development?"
+    policy = <<~HERE.chomp
+      # if Rails.env.development?
+      #   policy.connect_src *policy.connect_src,
+      #                     "ws://\#{ViteRuby.config.host_with_port}"
+      # end
+    HERE
+    # check if policy already exists
+    return if File.read(initializer).include?("policy.connect_src *policy.connect_src")
+
+    inject_into_file initializer, after: line do
+      policy
     end
   end
 
