eb48a36ce92dddffafe4d98bdda1581c2decf870
fprebolledo <fprebolledo@uc.cl>
2023-03-17 14:45:37 -0300

    feat(): delete rubocop runner where is not being used

--- spec/features/new_project_spec.rb
+++ spec/features/new_project_spec.rb
@@ -9,14 +9,8 @@ RSpec.describe "A new project" do
   end
 
   it "is correctly bundled" do
-    expect { on_project { `bundle exec rails -v` } }.to_not output.to_stderr
+    expect { on_project { `bundle exec rails -v` } }.not_to output.to_stderr
   end
-
-  # it "is a valid rubocop project" do
-    # on_project do
-    #   expect(run_rubocop).to eq(true)
-    # end
-  # end
 
   it "configures postgresql" do
     database_config_file = IO.read("#{project_path}/config/database.yml")
