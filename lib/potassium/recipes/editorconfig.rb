b67184b32815c9066a52d3694788190b1e7c39a7
Guillermo Moreno <gmq@guillermomoreno.cl>
2023-03-20 12:45:46 -0300

    feat(vscode): add section to readme

--- lib/potassium/recipes/editorconfig.rb
+++ lib/potassium/recipes/editorconfig.rb
@@ -3,5 +3,6 @@ class Recipes::Editorconfig < Rails::AppBuilder
     copy_file '../assets/.editorconfig', '.editorconfig'
     copy_file '../assets/.vscode/recommended-settings.json', '.vscode/recommended-settings.json'
     copy_file '../assets/.vscode/extensions.json', '.vscode/extensions.json'
+    add_readme_section :vscode
   end
 end
--- spec/features/editorconfig_spec.rb
+++ spec/features/editorconfig_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe "EditorConfig" do
 
   it { expect(File.exist?("#{project_path}/.editorconfig")).to be true }
   it { expect(Dir.entries("#{project_path}/.vscode")).to include(*conf_files) }
+
+  it "adds the vscode brief to README file" do
+    readme = IO.read("#{project_path}/README.md")
+    expect(readme).to include("VSCode Configuration")
+  end
 end
